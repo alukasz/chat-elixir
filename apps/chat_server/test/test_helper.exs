97bb4c964b3d373c2132ce2eab6573f5b445b2c6
Łukasz Antończyk <antonczyklukasz@gmail.com>
2017-12-03 17:39:45 +0100

    Chat.Server.TestHelper.run_in_background/1 resends messsages to test process

--- apps/chat_server/test/test_helper.exs
+++ apps/chat_server/test/test_helper.exs
@@ -1,6 +1,26 @@
 defmodule Chat.Server.TestHelper do
-  def run_in_background(fun) do
-    Agent.start_link(fun)
+  defmodule Echo do
+    use GenServer
+
+    def start_link(fun, process) do
+      GenServer.start_link(__MODULE__, {fun, process})
+    end
+
+    def init({fun, process}) do
+      fun.()
+
+      {:ok, process}
+    end
+
+    def handle_info(message, process) do
+      send(process, message)
+
+      {:noreply, process}
+    end
+  end
+
+  def run_in_background(fun, process \\ self()) do
+    Echo.start_link(fun, process)
   end
 end
 
